ae2ebe161d23e0b681a76b3950816c5cd53348f6
Mohammed Safaf <85129653+thedevsafaf@users.noreply.github.com>
2023-07-06 20:12:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.menu-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
